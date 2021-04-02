f966ce47fa915be01e0da867136e0d3b2948b5c4
Vladimir <verdant24@yandex.ru>
2021-04-02 16:06:08 +0300

    Добавили вывод постов

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -14,5 +15,16 @@ class Post extends Model
     public function user ()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        $date = Carbon::now();
+        $value = Carbon::create($value);
+
+        if ($date->format('F d, Y') == $value->format('F d, Y')) {
+            return $value->diffForHumans();
+        }
+        return $value->format('F d, Y');
     }
 }
